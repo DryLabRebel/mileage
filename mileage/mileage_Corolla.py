7d36ebf505937c6a3e7a9389b0d2cb1820d16152
Geoff English <geoffrey.english@uqconnect.edu.au>
2023-11-11 11:40:50 +1000

    rearranged directory to coform to python package conventions

--- mileage/mileage_Corolla.py
+++ mileage/mileage_Corolla.py
@@ -76,17 +76,66 @@ def mileage_report(data):
 
   print(data['date'].describe())
 
-#fuel_data = args.data
-fuel_data = "/Users/geoff/data/mileage_Captiva.txt"
+def mileage_calc(data):
+  """Calculates mean number of liters
+  """
+  # TODO: code an iterator which compares consecutive odometer readings
+    # any difference between non-null values, which is greater than one sd above sample mean is excluded
+      # Track these and record the number of excluded rows
+    # then simply (max-min)/(len(odo)+N.excluded)
+    # This should *mostly* correct for any missing odometer readings!
+    # NOTE: This also assumes you have only small fraction of missing readings (i.e. your sample mean has a small sample standard error)
+  # In summary - we simply want to know how many odometer readings have been missed, and add that to len(data)
+  odometer_min = fuel['odometer'].min()
+  odometer_max = fuel['odometer'].max()
+  mean_tank = int(round((odometer_max - odometer_min)/len(data), 0))
+  return mean_tank
+
+def petrol_L_calc(data):
+  # TODO: Ensure this calculation excludes NaN values
+  mean_petrol = int(round(data['petrol_L'].mean(),0))
+  return mean_petrol
+
+def petrol_L_per_100km(data):
+  tank = petrol_L_calc(fuel)
+  distance = mileage_calc(fuel)
+  L_per_100km = (tank/distance)*100
+  return L_per_100km
+
+def cost_per_km(data):
+  tank = petrol_L_calc(fuel)
+  distance = mileage_calc(fuel)
+  mean_price = int(round(data['cost_perL'].mean(), 0))
+  # (L/km)*mean(cost_perL)
+  cost_per_km = round((tank/distance)*mean_price, 2)
+  cost_per_100km = round(((tank/distance)*mean_price)*100, 2)
+  return cost_per_km, cost_per_100km 
+
+fuel_data = args.data
+#fuel_data = "/Users/geoff/data/mileage_Captiva.txt"
+#fuel_data = "/Users/geoff/data/mileage_Corolla.txt"
 fuel = pd.read_csv(fuel_data, sep="\t")
+
+tank = petrol_L_calc(fuel)
+distance = mileage_calc(fuel)
+L_per_100km = 
+mean_price_per_km = cost_per_km(fuel)
 
 mileage_report(fuel)
 
-# colnames
-# datatypes
+print(
+    "\nL per 100km:", L_per_100km,
+    "\nAverage price ($) per kilometer:", mean_price_per_km[0],
+    "\nAverage price ($) per 100 kilometers:", mean_price_per_km[1]
+    )
 
 # BOOM!
 # Now what?
+# Make it executable
+  # Print data summary
+
+# colnames
+# datatypes
 
 # Now, I can just wrangle the data
 
